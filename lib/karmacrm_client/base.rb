056a1efdda81f1d86ac6f866002a66524e74ff34
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-11-25 19:35:36 +0100

    Handle non-standard reponses
    ActiveResource expects collections to be returned in arrays. For
    some classes API returns results in the Hash instead (Company, for
    example) and we have to intercept response handling and extract
    results in a way ActiveResource can handle.

--- lib/karmacrm_client/base.rb
+++ lib/karmacrm_client/base.rb
@@ -2,28 +2,38 @@ require "active_resource"
 
 module KarmacrmClient
   class Base < ActiveResource::Base
-    def self.api_key=(api_key)
-      @@api_key = api_key
-    end
 
-    def self.api_key
-      @@api_key ||= ""
-    end
+    class << self
+      def api_key=(api_key)
+        @@api_key = api_key
+      end
 
-    def self.format
-      @@format ||= ActiveResource::Formats::JsonFormat
-    end
+      def api_key
+        @@api_key ||= ""
+      end
 
-    def self.config
-      yield self
-    end
-    
-    def self.find(*args)
-      scope = args.slice!(0)
-      options = args.slice!(0) || {}
-      options.merge!({:params => {:api_key => api_key}})
+      def format
+        @@format ||= ActiveResource::Formats::JsonFormat
+      end
+
+      def config
+        yield self
+      end
+
+      alias :instantiate_collection_orig :instantiate_collection
+
+      def instantiate_collection(collection, prefix_options = {})
+        collection = collection[collection_name] if collection.is_a? Hash
+        instantiate_collection_orig(collection, prefix_options)
+      end
       
-      super(scope, options)
+      def find(*args)
+        scope = args.slice!(0)
+        options = args.slice!(0) || {}
+        options.merge!({:params => {:api_key => api_key}})
+        
+        items = super(scope, options)
+      end
     end
     
   end
