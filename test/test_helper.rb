b713a38f54a6a24808b898a3a8d14f9cc6e6dee1
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-11-28 19:41:13 +0100

    Better handling configuration file loading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,10 @@ require "yaml"
 require "test/unit"
 require "karmacrm"
 
-raise "No configuration file. You must create test_config.yml" unless File.exist?("test_config.yml")
+cfg_file = File.join(File.expand_path("../", __FILE__), "test_config.yml")
+raise "No configuration file. You must create test_config.yml" unless File.exist?(cfg_file)
 
-test_cfg = YAML.load_file(File.join(File.expand_path("../", __FILE__), "test_config.yml"))
+test_cfg = YAML.load_file(cfg_file)
 Karmacrm::Base.config do |config|
   config.site = test_cfg[:site]
   config.api_key = test_cfg[:api_key]
