b7ae346cf1d974c49814a4bc790d9a92b22a986c
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-11-28 19:43:08 +0100

    Added more tests for Company entity

--- test/company_test.rb
+++ test/company_test.rb
@@ -12,5 +12,25 @@ module Karmacrm
       company.save
       assert_equal all_companies.length + 1, Company.find(:all).length
     end
+
+    def test_find
+      all_companies = Company.find(:all)
+      assert_not_nil all_companies.select {|cmp| cmp.name == 'Test company'}
+    end
+
+    def test_find_by_name
+      companies = Company.find(:all, :params => { :name => "Test company" })
+      assert_not_nil companies
+      assert companies.length > 0
+      assert companies[0].id > 0
+    end
+
+    def test_update
+      company = Company.find(:first, :params => { :name => "Test company" })
+      assert_not_nil company
+      company.name = "The other company"
+      assert company.save
+      assert_not_nil Company.find(:first, :params => { :name => "The other company" })
+    end
   end
 end
